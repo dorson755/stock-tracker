4d00d9ab849320f1502677b8c840bbb88c4a055e
Dorson Williams <68614535+dorson755@users.noreply.github.com>
2025-01-23 00:41:50 -0500

    Update app.py

--- stock-tracker/app.py
+++ stock-tracker/app.py
@@ -221,4 +221,4 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
